19dff387f7c23fecd41bdd5329d83b03ab0acae0
ShahriarArefin <arfin17310@gmail.com>
2024-04-30 22:37:43 +0600

    added add arts & crafts api

--- src/app.js
+++ src/app.js
@@ -23,6 +23,12 @@ app.use(express.json());
       const result = await db.collection("categories").insertMany(req.body);
       res.send(result.insertedIds);
    });
+
+   // ======== Add art & craft ========
+   app.post("/paintings/add", async (req, res) => {
+      const result = await db.collection("paintings").insertOne(req.body);
+      res.send(result.insertedId);
+   });
 })();
 
 module.exports = app;
