e01384616eb444b012427117ee35f059a42f63a7
ShahriarArefin <arfin17310@gmail.com>
2024-05-05 02:46:21 +0600

    added all arts & crafts related api(s) - add new painting - get a painting - get paintings - update a painting - delete a painting

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { ObjectId } = require("mongodb");
 const { connectToDB } = require("../db/database");
 
 const app = express();
@@ -13,6 +14,10 @@ app.use(express.json());
 (async () => {
    const db = await connectToDB();
 
+   // Collections
+   const categoriesCollection = await db.collection("categories");
+   const paintingsCollection = await db.collection("paintings");
+
    app.get("/", async (req, res) => {
       // fetch categories
       const categoriesCursor = await db.collection("categories").find();
@@ -20,16 +25,48 @@ app.use(express.json());
       res.send({ categories });
    });
 
-   // ======== Categories ========
+   // ======== Categories endpoints ========
    app.post("/category", async (req, res) => {
-      const result = await db.collection("categories").insertMany(req.body);
-      res.send(result.insertedIds);
+      const result = await categoriesCollection.insertOne(req.body);
+      res.send(result.insertedId);
    });
 
-   // ======== Add art & craft ========
-   app.post("/paintings/add", async (req, res) => {
-      const result = await db.collection("paintings").insertOne(req.body);
-      res.send(result.insertedId);
+   // ======== art & crafts endpoints ========
+   // ADD NEW PAINTING
+   app.post("/paintings", async (req, res) => {
+      const result = await paintingsCollection.insertOne(req.body);
+      res.send(result);
+   });
+
+   // GET A PAINTING
+   app.get("/paintings/:id", async (req, res) => {
+      const painting = await paintingsCollection.findOne({
+         _id: new ObjectId(req.params.id),
+      });
+      res.send(painting);
+   });
+
+   // GET PAINTINGS
+   app.get("/paintings", async (req, res) => {
+      const paintings = await paintingsCollection.find(req.query).toArray();
+      res.send(paintings);
+   });
+
+   // UPDATE A PAINTING
+   app.patch("/paintings/:id", async (req, res) => {
+      const result = await paintingsCollection.updateOne(
+         { _id: new ObjectId(req.params.id) },
+         { $set: req.body }
+      );
+      res.send(result);
+   });
+
+   // DELETE A PAINTING
+   app.delete("/paintings/:id", async (req, res) => {
+      const result = await paintingsCollection.deleteOne({
+         _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
    });
 })();
 
